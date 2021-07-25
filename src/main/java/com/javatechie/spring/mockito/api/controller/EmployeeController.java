8354ead412b45004a7ff60c0c3c15c5115ce0226
venkata subbareddy k <venkata subbareddy k@DESKTOP-QPNSBTO>
2021-07-25 17:57:09 +0530

    Initial commit

---
+++ src/main/java/com/javatechie/spring/mockito/api/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.javatechie.spring.mockito.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javatechie.spring.mockito.api.dao.EmployeeRepository;
+import com.javatechie.spring.mockito.api.model.Employee;
+import com.javatechie.spring.mockito.api.model.Response;
+
+@RestController
+@RequestMapping("/EmployeeService")
+public class EmployeeController {
+	@Autowired
+	private EmployeeRepository repository;
+//http://localhost:8080/EmployeeService/addEmployee
+	
+	@PostMapping("/addEmployee")
+	public Response addEmployee(@RequestBody Employee employee) {
+		repository.save(employee);
+		return new Response(employee.getId() + " inserted", Boolean.TRUE);
+	}
+//http://localhost:8080/EmployeeService/getEmployees
+	@GetMapping("/getEmployees")
+	public Response getAllEmployees() {
+		List<Employee> employees = repository.findAll();
+		return new Response("record counts : " + employees.size(), Boolean.TRUE);
+	}
+}
